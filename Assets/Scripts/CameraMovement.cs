1b24b297382a78bc61dde072e5e6a68d0cac65c4
Starc <Pottying1@gmail.com>
2026-08-31 00:45:41 +0700

    dang lam base

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 newPos = player.position;
+        newPos.z = transform.position.z;
+        transform.position = newPos;
+    }
+}
