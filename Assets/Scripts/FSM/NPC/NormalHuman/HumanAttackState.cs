722c6f4cf52b07a0b79b594df767805ec33a640c
Starc <Pottying1@gmail.com>
2026-09-19 21:58:15 +0700

    Lam rotation soldier

--- Assets/Scripts/FSM/NPC/NormalHuman/HumanAttackState.cs
+++ Assets/Scripts/FSM/NPC/NormalHuman/HumanAttackState.cs
@@ -1,3 +1,5 @@
+
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class HumanAttackState : HumanBaseState
@@ -5,16 +7,18 @@ public class HumanAttackState : HumanBaseState
     
     public override void EnterState(HumanStateManager humanState)
     {
-        Debug.Log("TAN CONG QUAI VAT");
+        Debug.Log("QUAI VAT!");
     }
 
     public override void UpdateState(HumanStateManager humanState)
     {
         float distance = Vector2.Distance(humanState.transform.position, humanState.playerController.transform.position);
-        
+        Vector2 direction = (Vector2)humanState.playerController.transform.position - (Vector2)humanState.transform.position;
+        float rotateAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         if (!humanState.playerController.isInvisible && distance < humanState.soldierAttackDis 
         && humanState.playerController.currentState != humanState.playerController.disguisedState)
         {
+            humanState.transform.rotation = Quaternion.Euler(0,0,rotateAngle);
             Debug.Log("BAN QUAI VAT!");
         }
     }
